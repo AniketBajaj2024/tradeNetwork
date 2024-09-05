5448ebb3f3b7712712318564c5413a7d52423d91
Aniket <bajajaniketmain@gmail.com>
2024-09-05 20:12:46 +0530

    database.init

--- src/database/database.js
+++ src/database/database.js
@@ -1,19 +1,16 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected successfully.");
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected successfully.');
   } catch (error) {
-    console.error("MongoDB connection error:", error.message);
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
-  }
+  } 
 };
 
 module.exports = connectDB;
